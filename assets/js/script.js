dac0e2c1be16fe2199e9074c3760149c4c66cea2
basidialsnail <dmmerchant@gmail.com>
2022-06-23 18:00:11 -0400

    Minor Bug: on save, removed all borders. Updated the save feature to add the boder-left back in.

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,7 @@ function createHourBlocks() {
         var hourBlockEl = $('<form>', {
             id: "08",
             class: "row time-block"
-        })
+        });
         var hourBlockHourEl = $('<label>',{
             class: "col-2 hour",
         });
@@ -60,45 +60,47 @@ function createHourBlocks() {
         var hourBlockBttnEl = $('<button>',{
             class: "col-1 saveBtn"
         });
-        $('<i class="fa fa-save"></i>').appendTo(hourBlockBttnEl)
-        hourBlockHourEl.text(moment(i, "HH").format("h:mm A"))
-        hourBlockHourEl.attr("time",i)
-        hourBlockInputEl.val(thisHourDetails.detail)
-        hourBlockHourEl.appendTo(hourBlockEl)
-        hourBlockInputEl.appendTo(hourBlockEl)
-        hourBlockBttnEl.appendTo(hourBlockEl)
-        hourBlockEl.appendTo(hourBlocksArea)
+        $('<i class="fa fa-save"></i>').appendTo(hourBlockBttnEl);
+        hourBlockHourEl.text(moment(i, "HH").format("h:mm A"));
+        hourBlockHourEl.attr("time",i);
+        hourBlockInputEl.val(thisHourDetails.detail);
+        hourBlockHourEl.appendTo(hourBlockEl);
+        hourBlockInputEl.appendTo(hourBlockEl);
+        hourBlockBttnEl.appendTo(hourBlockEl);
+        hourBlockEl.appendTo(hourBlocksArea);
     }
       
 }
 
 function getHourState(number) {
     if (number < currentTime) {
-        return "past"
+        return "past";
     } else if (number == currentTime) {
-        return "present"
+        return "present";
     } else {
-        return "future"
+        return "future";
     }
 }
 
 
 function saveItem(event) {
     event.preventDefault();
-    var target = $(event.target)
+    var target = $(event.target);
     if (event.target.matches(".fa")) {
         target = $(event.target).parent();
     }
-    var taskoutput = target.parent().children('textarea').val()
-    var timeoutput = target.parent().children('label').attr("time")
-    var indexUpdate = storedSchedules.findIndex(thisDetail => thisDetail.hour == timeoutput && thisDetail.day === selectedDay)
+    var taskoutput = target.parent().children('textarea').val();
+    var timeoutput = target.parent().children('label').attr("time");
+    var indexUpdate = storedSchedules.findIndex(thisDetail => thisDetail.hour == timeoutput && thisDetail.day === selectedDay);
     storedSchedules[indexUpdate].detail = taskoutput;
-    localStorage.setItem("allStoredSchedules", JSON.stringify(storedSchedules))
-    target.parent().children('textarea').css("border","none")
+    localStorage.setItem("allStoredSchedules", JSON.stringify(storedSchedules));
+    target.parent().children('textarea').css("border","none");
+    target.parent().children('textarea').css("border-left", "1px solid black");
 
 }
 function itemChanged(event) {
-    $(event.target).css("border","1px solid red")
+    $(event.target).css("border","1px solid red");
+
 }
 hourBlocksArea.on('click','.saveBtn',saveItem);
 hourBlocksArea.on('change','textarea',itemChanged);
