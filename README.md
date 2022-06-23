b05abc25605cefe97f2ca9892b69cd88ac4019b5
basidialsnail <dmmerchant@gmail.com>
2022-06-23 18:38:08 -0400

    updated readme and input comments into the code

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+```md
+This is a simple calendar app for scheduling your workday.
+```
+
+## Description
+
+```md
+This is a scheduler that has hour blocks for a normal workday schedule (8am - 5pm)
+When an hour block is blank, it will tell you that "Nothing is Scheduled"
+Each hour block is color coded based on the time of loading the page:
+    Grey = Past
+    Red = Present
+    Green = Future
+Changes can be made by updating the text block in the hour block and clicking on save.
+The block will outline red if there is an unsaved update.
+Data is stored for the next time the user opens the application within that day.
+
+The system is built to allow for a date selector (not added), that the user can update a specific day's schedule.
+```
+
+
+![Work Day Scheduler Image](./assets/media/screenshot.png)
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Getting Started
+[Launch Work Day Scheduler](https://dmmerchant.github.io/work-day-scheduler/)
+
+### Dependencies
+
+* Strictly HTML, CSS, and Javascript. No additional additional requirements
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,30 @@
+//Screen Element Variables
 var currentdayEl = $('#currentDay')
 var hourBlocksArea = $('#hourBlocks')
 
 
-
+//Global Variables
 var currentDay = moment().format("dddd, MMMM Do, YYYY");
-var currentTime = moment().format("H");
+var currentTime = moment("10","H").format("H");
 var selectedDay = moment().format("MM/DD/YY")
 var storedSchedules = [];
 
-
-
-
+//Set the current Day
 currentdayEl.text(currentDay);
 
+
+
+//Get stored data.
 function getStoredSchedule() {
     storedSchedules = JSON.parse(localStorage.getItem("allStoredSchedules"));
+    //On first run, create an array for the day.
     if (!storedSchedules) {
         storedSchedules = [];
         createBlankSchedule();
     }
 }
 
-
+//Creates a blank schedule for the selected day.
 function createBlankSchedule() {
     storedSchedules.push({day: selectedDay, hour:8, detail: "" })
     storedSchedules.push({day: selectedDay, hour:9, detail: "" })
@@ -34,44 +37,59 @@ function createBlankSchedule() {
     storedSchedules.push({day: selectedDay, hour:16, detail: "" })
     localStorage.setItem("allStoredSchedules", JSON.stringify(storedSchedules)) 
 }
-getStoredSchedule()
 
+//Creates the hour blocks. Filters all stored data for items for selectedDay. Loops through and inputs the details into the appropriate hour block.
 function createHourBlocks() {
     var todaySchedules = storedSchedules.filter(schedules => {
         return schedules.day == selectedDay});
+    //If there is stored data but not from the selected date, add a empty day for the selected date
     if (!todaySchedules) {
         createBlankSchedule();
         return createHourBlocks();
     };
-    for (var i = 8; i < 17; i++){ 
+    for (var i = 8; i < 17; i++){
+        //this finds the specific details for that hour
         var thisHourDetails = todaySchedules.find(hourDetails => hourDetails.hour === i)
+        //call the getHourState function to check if the block is past current or present
         var state = getHourState(i)
+        //This is the parent hour block element
         var hourBlockEl = $('<form>', {
             id: "08",
             class: "row time-block"
         });
+        //This is the element where the hour will display
         var hourBlockHourEl = $('<label>',{
             class: "col-2 hour",
         });
+        //This is the element where the user's hour details will display, along with the ability to update.
         var hourBlockInputEl = $('<textarea>',{
             class: "col " + state,
             placeholder: "Nothing Scheduled"
         });
+        //This is the save button, to store the changes the user makes
         var hourBlockBttnEl = $('<button>',{
             class: "col-1 saveBtn"
         });
+        //this is the save icon within the save button
         $('<i class="fa fa-save"></i>').appendTo(hourBlockBttnEl);
+
+        //utilizes the i to format a proper time stamp
         hourBlockHourEl.text(moment(i, "HH").format("h:mm A"));
+        //stores the i as an element to be used when saving
         hourBlockHourEl.attr("time",i);
+        //update the detail block
         hourBlockInputEl.val(thisHourDetails.detail);
+        //Append the elements to the hour block
         hourBlockHourEl.appendTo(hourBlockEl);
         hourBlockInputEl.appendTo(hourBlockEl);
         hourBlockBttnEl.appendTo(hourBlockEl);
+        //Append to the screen
         hourBlockEl.appendTo(hourBlocksArea);
     }
       
 }
 
+//checks whether the time is current or past
 function getHourState(number) {
     if (number < currentTime) {
         return "past";
@@ -82,7 +100,7 @@ function getHourState(number) {
     }
 }
 
-
+//update the saved data
 function saveItem(event) {
     event.preventDefault();
     var target = $(event.target);
@@ -91,18 +109,26 @@ function saveItem(event) {
     }
     var taskoutput = target.parent().children('textarea').val();
     var timeoutput = target.parent().children('label').attr("time");
+    //find the index in the array of all saved data. Must do this as to not clear out other information.
     var indexUpdate = storedSchedules.findIndex(thisDetail => thisDetail.hour == timeoutput && thisDetail.day === selectedDay);
     storedSchedules[indexUpdate].detail = taskoutput;
     localStorage.setItem("allStoredSchedules", JSON.stringify(storedSchedules));
+
+    //removes the notification that the data is unsaved
     target.parent().children('textarea').css("border","none");
     target.parent().children('textarea').css("border-left", "1px solid black");
-
 }
+
+//This is to notify that the user has unsaved data
 function itemChanged(event) {
     $(event.target).css("border","1px solid red");
 
 }
+
+//Start Up Routine
+getStoredSchedule()
+createHourBlocks()
+
+//Event Listeners
 hourBlocksArea.on('click','.saveBtn',saveItem);
 hourBlocksArea.on('change','textarea',itemChanged);
-
-createHourBlocks()
